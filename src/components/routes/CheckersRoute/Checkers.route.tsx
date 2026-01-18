e98e1233f099b435afac520f001bf311e21a833e
Darcy Driscoll <darcy.driscoll@xero.com>
2026-01-18 14:45:16 +1100

    Add piece moving ability

--- src/components/routes/CheckersRoute/Checkers.route.tsx
+++ src/components/routes/CheckersRoute/Checkers.route.tsx
@@ -1,23 +1,78 @@
-import { selectAvailableMoves, selectSquares } from "@/features/checkers";
-import { useAppSelector } from "@/hooks";
-import { useEffect, useState } from "react";
+import {
+  doMoves,
+  selectAvailableMoves,
+  selectPiece,
+  selectSquares,
+  type DoMovesPayload,
+  type Move,
+} from "@/features/checkers";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { useState } from "react";
 
 export const CheckersRoute = () => {
+  const dispatch = useAppDispatch();
   const checkers = useAppSelector(selectSquares);
   const rows = checkers.length;
   const cols = checkers[0].length;
 
-  const [selected, setSelected] = useState({ row: null, col: null } as {
+  const [selectedPieceCoords, setSelectedPieceCoords] = useState({
+    row: null,
+    col: null,
+  } as {
     row: number | null;
     col: number | null;
   });
 
+  const selectedPiece = useAppSelector((state) =>
+    selectPiece(state, selectedPieceCoords.row, selectedPieceCoords.col),
+  );
+
+  const [selectedMoves, setSelectedMoves] = useState([] as Move[]);
+
   const availableMoves = useAppSelector((state) =>
-    selectAvailableMoves(state, selected.row, selected.col)
+    selectAvailableMoves(
+      state,
+      selectedPieceCoords.row,
+      selectedPieceCoords.col,
+      selectedPiece,
+      [],
+    ),
+  );
+
+  const availableMovesFromLastSelectedMove = useAppSelector((state) =>
+    selectAvailableMoves(
+      state,
+      selectedMoves[selectedMoves.length - 1]?.row ?? null,
+      selectedMoves[selectedMoves.length - 1]?.col ?? null,
+      selectedPiece,
+      selectedMoves,
+    ),
   );
 
   return (
     <section>
+      <button
+        className="bg-white cursor-pointer p-2 mb-4"
+        onClick={() => {
+          if (
+            selectedPieceCoords.row !== null &&
+            selectedPieceCoords.col !== null &&
+            selectedMoves.length > 0
+          ) {
+            dispatch(
+              doMoves({
+                row: selectedPieceCoords.row,
+                col: selectedPieceCoords.col,
+                moves: selectedMoves,
+              } as DoMovesPayload),
+            );
+            setSelectedMoves([]);
+            setSelectedPieceCoords({ row: null, col: null });
+          }
+        }}
+      >
+        Do moves
+      </button>
       <div
         style={
           {
@@ -30,23 +85,67 @@ export const CheckersRoute = () => {
         {checkers
           .map((row, i) => {
             return row.map((square, j) => {
-              const isSelected = i === selected.row && j === selected.col;
-              const squareIsAvailable = availableMoves.some(
-                (move) => move.row === i && move.col === j
+              const isFirstMove = selectedMoves.length === 0;
+              const isLastMoveCapture =
+                selectedMoves.length !== 0 &&
+                selectedMoves[selectedMoves.length - 1].effect.kind ===
+                  "capture";
+
+              const selectedMove = selectedMoves.find(
+                (selectedMove) =>
+                  selectedMove.row === i && selectedMove.col === j,
               );
+              const isPositionOfSelectedMove = selectedMove !== undefined;
+
+              const isSelectedPiece =
+                i === selectedPieceCoords.row && j === selectedPieceCoords.col;
+
+              const move = isFirstMove
+                ? availableMoves.find(
+                    (availableMove) =>
+                      availableMove.row === i && availableMove.col === j,
+                  )
+                : availableMovesFromLastSelectedMove.find(
+                    (availableMove) =>
+                      availableMove.row === i && availableMove.col === j,
+                  );
+              const isAvailableMove = move !== undefined;
+
+              const canMoveHere =
+                !isSelectedPiece &&
+                isAvailableMove &&
+                (isFirstMove || isLastMoveCapture);
+
+              if (canMoveHere) {
+                console.log("foo");
+              }
+
               const style =
                 square.piece === undefined
-                  ? `${squareIsAvailable ? "bg-emerald-500" : "bg-gray-500"}`
+                  ? `${
+                      canMoveHere || isPositionOfSelectedMove
+                        ? "bg-emerald-500"
+                        : "bg-gray-500"
+                    }`
                   : square.piece.player === "PLAYER_1"
-                  ? `${isSelected ? "bg-rose-500" : "bg-amber-300"}`
-                  : `${isSelected ? "bg-rose-500" : "bg-blue-400"}`;
+                    ? `${isSelectedPiece ? "bg-rose-500" : "bg-amber-300"}`
+                    : `${isSelectedPiece ? "bg-rose-500" : "bg-blue-400"}`;
+
               return (
                 <div
                   className={`${style} w-12 h-12`}
                   onClick={
                     square.piece !== undefined
-                      ? () => setSelected({ row: i, col: j })
-                      : undefined
+                      ? () => {
+                          setSelectedMoves([]);
+                          setSelectedPieceCoords({ row: i, col: j });
+                        }
+                      : canMoveHere
+                        ? () => setSelectedMoves([...selectedMoves, move])
+                        : () => {
+                            setSelectedMoves([]);
+                            setSelectedPieceCoords({ row: null, col: null });
+                          }
                   }
                 ></div>
               );
--- src/features/checkers/checkers.slice.ts
+++ src/features/checkers/checkers.slice.ts
@@ -1,4 +1,8 @@
-import { createSelector, createSlice } from "@reduxjs/toolkit";
+import {
+  createSelector,
+  createSlice,
+  type PayloadAction,
+} from "@reduxjs/toolkit";
 import type { RootState } from "../../redux/store";
 
 export const PLAYER_ENUM = {
@@ -74,13 +78,75 @@ const initialState: CheckersState = {
   ],
 };
 
+export interface DoMovesPayload {
+  row: number;
+  col: number;
+  moves: Move[];
+}
+
+export interface Effect {
+  kind: "null" | "capture";
+  row: number;
+  col: number;
+}
+
+export interface NullEffect extends Effect {
+  kind: "null";
+}
+
+export interface CaptureEffect extends Effect {
+  kind: "capture";
+  row: number;
+  col: number;
+}
+
+export interface Move {
+  row: number;
+  col: number;
+  effect: Effect;
+}
+
 export const checkersSlice = createSlice({
   name: "checkers",
   initialState,
-  reducers: {},
+  reducers: {
+    doMoves: (state, action: PayloadAction<DoMovesPayload>) => {
+      const payload = action.payload;
+      let currentRow = payload.row;
+      let currentCol = payload.col;
+      payload.moves.forEach((move) => {
+        const nextRow = move.row;
+        const nextCol = move.col;
+        const squares = state.squares;
+        squares[nextRow][nextCol] = squares[currentRow][currentCol];
+        squares[currentRow][currentCol] = { piece: undefined };
+
+        if (move.effect.kind === "capture") {
+          squares[move.effect.row][move.effect.col] = { piece: undefined };
+        }
+
+        currentRow = nextRow;
+        currentCol = nextCol;
+      });
+    },
+  },
 });
 
 export const selectSquares = (state: RootState) => state.checkers.squares;
+
+export const selectPiece = createSelector(
+  [
+    selectSquares,
+    (_, row: number | null) => row,
+    (_, _row: number | null, col: number | null) => col,
+  ],
+  (squares, row, col) => {
+    if (row === null || col === null) {
+      return null;
+    }
+    return squares[row][col].piece ?? null;
+  },
+);
 
 const isMoveOutOfBounds = (squares: Square[][], row: number, col: number) => {
   return (
@@ -93,8 +159,11 @@ const getMoveOrUndefined = (
   verticalDirection: "UP" | "DOWN",
   horizontalDirection: "LEFT" | "RIGHT",
   currentRow: number,
-  currentCol: number
+  currentCol: number,
+  piece: Piece,
 ) => {
+  const player = piece.player;
+
   const verticalSign = verticalDirection === "UP" ? -1 : 1;
   const horizontalSign = horizontalDirection === "LEFT" ? -1 : 1;
 
@@ -105,73 +174,126 @@ const getMoveOrUndefined = (
 
   if (!isMoveOutOfBounds(squares, move.row, move.col)) {
     if (squares[move.row][move.col].piece === undefined) {
-      return move;
-    } else {
+      return {
+        row: move.row,
+        col: move.col,
+        effect: {} as NullEffect,
+      } as Move;
+    } else if (squares[move.row][move.col].piece?.player !== player) {
+      const takenRow = move.row;
+      const takenCol = move.col;
       move.row = move.row + verticalSign;
       move.col = move.col + verticalSign * horizontalSign;
       if (!isMoveOutOfBounds(squares, move.row, move.col)) {
         if (squares[move.row][move.col].piece === undefined) {
-          return move;
+          const effect: CaptureEffect = {
+            kind: "capture",
+            row: takenRow,
+            col: takenCol,
+          };
+          return {
+            row: move.row,
+            col: move.col,
+            effect,
+          } as Move;
         }
       }
     }
   }
 };
 
-export const selectAvailableMoves = createSelector(
-  [
-    selectSquares,
-    (_, currentRow: number | null) => currentRow,
-    (_, _currentRow: number | null, currentCol: number | null) => currentCol,
-  ],
-  (
-    squares: Square[][],
-    currentRow: number | null,
-    currentCol: number | null
-  ) => {
-    if (currentRow === null || currentCol === null) return [];
+export const selectAvailableMoves = (
+  state: RootState,
+  currentRow: number | null,
+  currentCol: number | null,
+  piece: Piece | null,
+  previousMoves: Move[],
+) => {
+  const isPreviousMovesEmpty = previousMoves.length === 0;
+  const wasLastMoveACapture =
+    !isPreviousMovesEmpty &&
+    previousMoves[previousMoves.length - 1].effect.kind === "capture";
 
-    const piece = squares[currentRow][currentCol].piece;
+  if (
+    currentRow === null ||
+    currentCol === null ||
+    piece === null ||
+    (!isPreviousMovesEmpty && !wasLastMoveACapture)
+  )
+    return [];
 
-    if (piece === undefined) {
-      throw new Error(`Empty space, row: ${currentRow}, col: ${currentCol}`);
-    }
+  const squares = selectSquares(state);
 
-    const availableMoves = [];
+  const availableMoves: Move[] = [];
 
-    if (piece.pieceType === "MAN") {
-      const verticalDirection = piece.player === "PLAYER_1" ? "UP" : "DOWN";
-      availableMoves.push(
-        ...[
-          getMoveOrUndefined(
-            squares,
-            verticalDirection,
-            "LEFT",
-            currentRow,
-            currentCol
-          ),
-          getMoveOrUndefined(
-            squares,
-            verticalDirection,
-            "RIGHT",
-            currentRow,
-            currentCol
-          ),
-        ].filter((x) => x !== undefined)
-      );
-    } else if (piece.pieceType === "KING") {
-      availableMoves.push(
-        ...[
-          getMoveOrUndefined(squares, "UP", "LEFT", currentRow, currentCol),
-          getMoveOrUndefined(squares, "UP", "RIGHT", currentRow, currentCol),
-          getMoveOrUndefined(squares, "DOWN", "LEFT", currentRow, currentCol),
-          getMoveOrUndefined(squares, "DOWN", "RIGHT", currentRow, currentCol),
-        ].filter((x) => x !== undefined)
-      );
-    }
-
-    return availableMoves;
+  if (piece.pieceType === "MAN") {
+    const verticalDirection = piece.player === "PLAYER_1" ? "UP" : "DOWN";
+    availableMoves.push(
+      ...[
+        getMoveOrUndefined(
+          squares,
+          verticalDirection,
+          "LEFT",
+          currentRow,
+          currentCol,
+          piece,
+        ),
+        getMoveOrUndefined(
+          squares,
+          verticalDirection,
+          "RIGHT",
+          currentRow,
+          currentCol,
+          piece,
+        ),
+      ].filter((move) => move !== undefined),
+    );
+  } else if (piece.pieceType === "KING") {
+    availableMoves.push(
+      ...[
+        getMoveOrUndefined(
+          squares,
+          "UP",
+          "LEFT",
+          currentRow,
+          currentCol,
+          piece,
+        ),
+        getMoveOrUndefined(
+          squares,
+          "UP",
+          "RIGHT",
+          currentRow,
+          currentCol,
+          piece,
+        ),
+        getMoveOrUndefined(
+          squares,
+          "DOWN",
+          "LEFT",
+          currentRow,
+          currentCol,
+          piece,
+        ),
+        getMoveOrUndefined(
+          squares,
+          "DOWN",
+          "RIGHT",
+          currentRow,
+          currentCol,
+          piece,
+        ),
+      ].filter((move) => move !== undefined),
+    );
   }
-);
+
+  if (wasLastMoveACapture) {
+    return availableMoves.filter((move) => move.effect.kind === "capture");
+  }
+
+  return availableMoves;
+};
+
+export const { doMoves } = checkersSlice.actions;
 
 export const checkersReducer = checkersSlice.reducer;
